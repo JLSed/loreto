ea0c95396f2fdc859afb3e4d32093405e56246dc
ericechemane <eechemane29@gmail.com>
2024-03-15 08:41:39 +0800

    layout controls

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,145 @@
-import Image from "next/image";
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable'
+import { Slider } from '@/components/ui/slider'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  // actual pixel
+  const [height, setHeight] = useState(400)
+  const [pixelWidth, setPixelWidth] = useState(0)
+  const [pixelLength, setPixelLength] = useState(0)
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const [containerWidth, setContainerWidth] = useState(0)
+
+  // percentage
+  const [widthPercentage, setWidthPercentage] = useState(25)
+  const [lengthPercentage, setLengthPercentage] = useState(35)
+
+  useEffect(() => {
+    const w = document.getElementById('main-container')?.clientWidth || 0
+    setContainerWidth(w)
+    setPixelWidth(Math.round((w * widthPercentage) / 100))
+    setPixelLength(Math.round((w * lengthPercentage) / 100))
+  }, [lengthPercentage, widthPercentage])
+
+  return (
+    <main className='grid grid-cols-[250px_1fr] pr-4 py-4'>
+      <div className='flex flex-col p-4 pt-0 gap-4'>
+        <div className='border-b py-3 flex items-center justify-between'>
+          <div className='text-muted-foreground small'>Controls</div>
+          <Button size={'sm'}>Apply</Button>
+        </div>
+        <div className='flex items-center justify-between'>
+          <label
+            htmlFor='Width'
+            className='small'
+          >
+            Width
+          </label>
+          <Input
+            id='Width'
+            type='number'
+            placeholder='Width'
+            defaultValue={pixelWidth}
+            className='w-24'
+          />
+        </div>
+        <div className='flex items-center justify-between'>
+          <label
+            htmlFor='Length'
+            className='small'
+          >
+            Length
+          </label>
+          <Input
+            id='Length'
+            type='number'
+            placeholder='Length'
+            defaultValue={pixelLength}
+            className='w-24'
+          />
+        </div>
+        <div className='flex items-center justify-between'>
+          <label
+            htmlFor='Height'
+            className='small'
+          >
+            Height
+          </label>
+          <Input
+            type='number'
+            placeholder='Height'
+            key={height}
+            defaultValue={height}
+            className='w-24'
+            onChange={(e) => {
+              let value = +e.currentTarget.value
+              if (value < 100 || value > 1000) return
+              setHeight(value)
+            }}
+          />
+        </div>
+        <Slider
+          value={[height]}
+          max={1000}
+          onValueChange={(value) => setHeight(value[0])}
+          step={1}
+          className='w-full'
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <ResizablePanelGroup
+        id={'main-container'}
+        direction='horizontal'
+        className='rounded-lg border py-8'
+        style={{
+          height: `${height}px`,
+        }}
+      >
+        <ResizablePanel defaultSize={20} />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <ResizableHandle withHandle />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <ResizablePanel
+          className='bg-yellow-900/45 relative'
+          defaultValue={widthPercentage}
+          onResize={setWidthPercentage}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <div className='absolute bottom-0 left-1/2 -translate-x-1/2 p-4 flex items-center gap-2'>
+            W - {pixelWidth}
+          </div>
+        </ResizablePanel>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <ResizableHandle withHandle />
+
+        <ResizablePanel
+          className='bg-yellow-900/30 relative'
+          defaultSize={lengthPercentage}
+          onResize={setLengthPercentage}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <div className='absolute bottom-0 left-1/2 -translate-x-1/2 p-4 flex items-center gap-2'>
+            L - {pixelLength}
+          </div>
+        </ResizablePanel>
+
+        <ResizableHandle withHandle />
+
+        <ResizablePanel
+          defaultSize={20}
+          className='relative'
+        >
+          <div className='absolute left-0 top-1/2 -translate-y-1/2 p-4'>
+            H: {height}
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </main>
-  );
+  )
 }
