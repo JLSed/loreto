020782760267db56bb4cabfad160b3e4e477ea30
ericechemane <eechemane29@gmail.com>
2024-04-06 15:08:51 +0800

    able to move markings and save its position

--- src/app/(main)/box/_markings/Phase1.tsx
+++ src/app/(main)/box/_markings/Phase1.tsx
@@ -1,7 +1,9 @@
+'use client'
+
 import useBoxControls from '../useBoxControls'
 
 import Moveable from 'react-moveable'
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { flushSync } from 'react-dom'
 
 interface Props {
@@ -12,6 +14,22 @@ export default function Phase1(props: Props) {
   const targetRef = useRef<HTMLDivElement>(null)
   const containerRef = useRef<HTMLDivElement>(null)
 
+  const [draggable, setDraggable] = useState(false)
+  const [transformationsDone, setTransformationsDone] = useState(false)
+
+  const getTransformFromLocalStorage = () => {
+    if (typeof window === 'undefined') return
+    return localStorage.getItem('transform')
+  }
+
+  useEffect(() => {
+    const t = getTransformFromLocalStorage()
+    if (targetRef.current && t) {
+      targetRef.current.style.transform = t
+      setTransformationsDone(true)
+    }
+  }, [])
+
   return (
     <>
       <div
@@ -19,20 +37,29 @@ export default function Phase1(props: Props) {
         ref={containerRef}
       >
         <div
+          hidden={!transformationsDone}
+          onMouseOver={() => setDraggable(true)}
+          onMouseUp={() => setDraggable(false)}
           ref={targetRef}
-          className='p-2'
+          className='p-2 cursor-move'
         >
           Marking
         </div>
       </div>
 
       <Moveable
-        draggable
+        clickable
+        key={props.controls.widthPercentage + transformationsDone.toString()}
+        draggable={draggable}
+        hideDefaultLines={!draggable}
         flushSync={flushSync}
         target={targetRef.current}
+        origin={false}
         // container={containerRef.current}
         onDrag={(e) => {
           e.target.style.transform = e.transform
+          localStorage.setItem('transform', e.transform)
+          console.log(e.transform)
         }}
         onResize={(e) => {
           e.target.style.width = `${e.width}px`
