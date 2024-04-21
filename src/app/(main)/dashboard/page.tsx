74dd121c86e90b209ee6ad194515d6cd47755f44
ericechemane <eechemane29@gmail.com>
2024-04-21 12:06:52 +0800

    remove logs in dashboard

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,20 +1,11 @@
 import PageUnderConstruction from '@/components/shared/PageUnderConstruction'
 
 import { prisma } from '@/common/configs/prisma'
-import {
-  AuditAction,
-  AuditAffectedTable,
-  UserRole,
-  UserStatus,
-} from '@/common/enums/enums.db'
+import { UserRole, UserStatus } from '@/common/enums/enums.db'
 import MaterialIcon, { MaterialIconName } from '@/components/ui/material-icon'
-import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Card } from '@/components/ui/card'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { ReactNode } from 'react'
-import { ScrollArea } from '@/components/ui/scroll-area'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { Badge } from '@/components/ui/badge'
-import { ArrowRightIcon } from '@radix-ui/react-icons'
 
 export default async function Page() {
   const [boxOrders, bookings, transactions, activeCustomers, auditLogs] =
@@ -80,73 +71,6 @@ export default async function Page() {
               value={activeCustomers.length}
               materialIconName={'group'}
             />
-          </div>
-
-          <div className='grid grid-cols-12 gap-4'>
-            <Card className='mt-4 col-span-8'>
-              <CardHeader className='small'>Most Recent Logs</CardHeader>
-              <CardContent>
-                <ScrollArea className='max-h-[320px] overflow-y-visible'>
-                  <ul>
-                    {auditLogs.map((log) => {
-                      const action =
-                        log.action === AuditAction.Creation
-                          ? 'created'
-                          : 'modified'
-
-                      return (
-                        <li
-                          key={log.id}
-                          className='py-1'
-                        >
-                          <div className='flex items-center'>
-                            <Avatar className='scale-50'>
-                              <AvatarImage
-                                src={log.actor.photoUrl ?? ''}
-                                alt=''
-                              />
-                              <AvatarFallback>
-                                {log.actor.username[0]}
-                              </AvatarFallback>
-                            </Avatar>
-                            <div className='capitalize mr-4 text-sm max-w-[200px] truncate text-ellipsis'>
-                              {log.actor.username}
-                            </div>
-                            <span className='text-sm'>{action}</span>
-                            <span className='text-muted-foreground mx-4'>
-                              {AuditAffectedTable[log.affectedTable]}
-                            </span>
-                            {log.action === AuditAction.Modification && (
-                              <>
-                                <Badge
-                                  className='capitalize'
-                                  variant={'outline'}
-                                >
-                                  {log.columnName}
-                                </Badge>
-                                <Badge
-                                  className='mx-4'
-                                  variant={'secondary'}
-                                >
-                                  {log.from}
-                                </Badge>
-                                <ArrowRightIcon />
-                                <Badge
-                                  className='ml-4'
-                                  variant={'secondary'}
-                                >
-                                  {log.to}
-                                </Badge>
-                              </>
-                            )}
-                          </div>
-                        </li>
-                      )
-                    })}
-                  </ul>
-                </ScrollArea>
-              </CardContent>
-            </Card>
           </div>
         </TabsContent>
 
